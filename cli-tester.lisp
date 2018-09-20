b2cf70d37c6d1d86088c0e0b80e6e270d428d9db
Matthew Hitchman <m.hitchman@outlook.com>
2018-09-20 17:03:16 +0100

    print number of passed and failed

--- cli-tester.lisp
+++ cli-tester.lisp
@@ -3,6 +3,7 @@
 (in-package #:cli-tester)
 
 (defparameter *program-to-run* nil)
+(defparameter *test-list* ())
 
 (defun register-program (program)
   #| check that the program exists
@@ -10,10 +11,22 @@
   (setf *program-to-run* program))
 
 (defun check (args expected-result)
-  (string= expected-result (typecase args
-			     (list (apply #'run-and-return-result args))
-			     (null (funcall #'run-and-return-result))
-			     (otherwise (funcall #'run-and-return-result args)))))
+  (push (list args expected-result) *test-list*))
+
+(defun run-tests ()
+  (setf *test-list* (nreverse *test-list*))
+  (let ((passed 0)
+	(failed 0))
+    (dolist (test-case *test-list*)
+      (let ((args (first test-case))
+	    (expected-result (second test-case)))
+	(if (string= expected-result (typecase args
+				       (list (apply #'run-and-return-result args))
+				       (null (funcall #'run-and-return-result))
+				       (otherwise (funcall #'run-and-return-result args))))
+	    (incf passed)
+	    (incf failed))))
+    (format t "Passed: ~a~%Failed: ~a~%" passed failed)))
 
 (defun run-and-return-result (&rest args)
   (let ((stream (uiop:process-info-output (uiop:launch-program `(,*program-to-run* ,@args) :output :stream)))
